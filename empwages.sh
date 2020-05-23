7a4c1f684668dc2360e516a09b27e3398a712b39
Shubham <s.shrinath@splendornet.com>
2020-05-23 23:30:04 +0530

    Employee Wage using Case Statement

--- empwages.sh
+++ empwages.sh
@@ -6,17 +6,17 @@ EMP_RATE_PERHR=20;
 isPartTime=1;
 isFullTime=2;
 
-randomCheck=$(( RANDOM%3 ))
+empCheck=$(( RANDOM%3 ))
 
-if [[ $randomCheck -eq $isFullTime ]];
- then
-		empHrs=8;
-
-elif [[ $randomCheck -eq $isPartTime ]];
- then
-		empHrs=4;
-else
-		empHrs=0;			
-fi
-
+case empCheck in
+	isFullTime)
+		 empHrs=8
+		 ;;
+	isPartTime)
+		 empHrs=8
+		  ;;
+        *)     
+		empHrs=8
+		;;	
+esac
 salary=$((EMP_RATE_PERHR*empHrs));
