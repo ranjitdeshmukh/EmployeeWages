679423fe002894cfe541125bdae394dd98835c76
Shubham <s.shrinath@splendornet.com>
2020-05-23 22:57:41 +0530

    Added:Employee daily wages calculation functionality

--- empwages.sh
+++ empwages.sh
@@ -2,13 +2,17 @@
 
 echo "Welcome TO Employee Wages Problem"
 
+EMP_RATE_PERHR=20;
+EMP_HR=8;
+
 randomCheck=$(( RANDOM%2 ))
 
 if [[ $randomCheck -eq 1 ]];
  then
 	echo "Employee is  present"
+	salary=$((EMP_RATE_PERHR*EMP_HR));
 
 elif [[ $randomCheck -eq 0 ]];
  then
-		echo "Employee not present"	
+	echo "Employee not present"	
 fi
