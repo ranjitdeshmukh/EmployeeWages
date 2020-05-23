a39b26dd779d7dba2168beb81b47d47708225bc5
Shubham <s.shrinath@splendornet.com>
2020-05-23 22:44:56 +0530

    Added:Employee Attendance functionality

--- empwages.sh
+++ empwages.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
+
 echo "Welcome TO Employee Wages Problem"
+
+randomCheck=$(( RANDOM%2 ))
+
+if [[ $randomCheck -eq 1 ]];
+ then
+	echo "Employee is  present"
+
+elif [[ $randomCheck -eq 0 ]];
+ then
+		echo "Employee not present"	
+fi
