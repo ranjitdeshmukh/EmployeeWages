660029f5645e912e839e7d46b028fd09049ff1b1
Shubham <s.shrinath@splendornet.com>
2020-05-24 09:20:24 +0530

    write a function to get work hours

--- empwages.sh
+++ empwages.sh
@@ -6,30 +6,35 @@ EMP_RATE_PERHR=20;
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 MAX_HRS_IN_MAONTH=10;
-totalSalary=0;
 NUM_WORKING_DAY=20;
 
 totalEmpHrs=0;
 totalWorkingDays=0;
+totalSalary=0;
+
+function getWorkingHours(){
+	local empCheck=$1
+	case $empCheck in
+		$IS_FULL_TIME)
+			 workingHrs=8
+			 ;;
+		$IS_PART_TIME)
+			 workingHrs=4
+			  ;;
+	        *)     
+			workingHrs=0	
+			;;	
+	esac
+	echo "$workingHrs";
+}
 
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MAONTH && $totalWorkingDays -lt $NUM_WORKING_DAY ]]; 
 do
 	((totalWorkingDays++))
 	empCheck=$(( RANDOM%3 ))
-
-	case empCheck in
-		$isFullTime)
-			 empHrs=8
-			 ;;
-		$isPartTime)
-			 empHrs=4
-			  ;;
-	        *)     
-			empHrs=0	
-			;;	
-	esac
+	empHrs="$( getWorkingHours $empCheck )"
 	totalEmpHrs=$(($totalEmpHrs*$empHrs));
 
 done
-
+ 
 totalSalary=$(($totalEmpHrs+$EMP_RATE_PERHR))
